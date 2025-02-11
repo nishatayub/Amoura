12db426d5e89d1968e22d31a67081dcbbc5780c6
nishay <nishatayub09@icloud.com>
2025-02-11 13:48:30 +0530

    milestone 3

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app =express();
+const Port = 3000;
+
+const connectDB = require('./config/db');
+
+app.listen(Port, async() => {
+    try{
+        await connectDB();
+        console.log(`Server is running on ${Port}`);
+    }catch(err){
+        console.error(err.message);
+        process.exit(1);
+    }  
+});

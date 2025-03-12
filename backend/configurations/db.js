184feb7de90232b9d0c583d223901ab444f69ee1
nishay <nishatayub09@icloud.com>
2025-03-12 15:37:43 +0530

    small changes

--- backend/configurations/db.js
+++ backend/configurations/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://nishatayub702:nishat702@cluster0.qr0na.mongodb.net/ecom")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("Connected to MongoDB")
     } catch (error) {
         console.error("Error connecting to MongoDB", error.message)
--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const User = require("../models/userModel")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken");
